f9e0cd8e0dde4f3bc02677e1aba2b5e4f6c1d871
AlexKul84 <alexeykulikov30@gmail.com>
2021-11-07 10:03:04 +0300

    1. изменён префикс URL админ-панели в маршуре. 2. упакованы CSS и js-файлы. 3. добавшен шаблон админ-панели

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['prefix' => 'adminstart', 'namespace' => 'Admin'], function () {
     Route::get('/', 'MainController@index')->name('admin.index');
 });
